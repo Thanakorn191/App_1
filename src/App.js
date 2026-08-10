7ff79709e7283ce1cca8d9e61c8cbffbf4bf527e
Thanakorn191 <farmhousefarmhouse006@gmail.com>
2026-08-10 09:57:13 +0700

    update RefsFunc

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Content, Footer, Header } from './func-components';
-import Calendar from './class-components';
-import EventData2 from './event-data';
-import Calculator from './calculator';
+
+import RefsFunc from './refs-func';
 
 function App() {
   
-  return <EventData2/>;
+  return <RefsFunc/>;
 
 }
 
