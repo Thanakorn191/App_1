cac60d3d00d0cdc0ac6e049cdb8f693dc8b3c7ac
Thanakorn191 <farmhousefarmhouse006@gmail.com>
2026-08-10 11:23:42 +0700

    create state-func

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-import RefsFunc from './refs-func';
-
-import RefsArray from './refs-array';
+import StateFunc from './state-func';
 
 function App() {
   
-  return <RefsArray/>;
+  return <StateFunc/>;
 
 }
 
